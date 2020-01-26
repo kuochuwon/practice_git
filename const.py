2973e1b59dff52f36497ca6fa34ac6c68cdb0e6b
kuochuwon <42232671+kuochuwon@users.noreply.github.com>
2020-01-26 21:30:16 +0800

    temp

--- const.py
+++ const.py
@@ -1,5 +1,16 @@
-Cookie = 'SPC_F=2c95sYFkzfWrz1RZ0pMLLlug1rPm9S0o; REC_T_ID=fc9cb468-d880-11e9-b087-f8f21e1a7612; _ga=GA1.2.741601534.1568638412; __BWfp=c1568638413405xf26e6cced; cto_lwid=b0ccf836-615e-4837-8c84-a7d7c1705a99; SPC_IA=-1; G_ENABLED_IDPS=google; _med=refer; _gcl_au=1.1.122690841.1576505462; _fbp=fb.1.1578399168664.1918442970; csrftoken=BZqH8wT7HRzYLozH9s4XC3fdPORCycaX; CTOKEN=XKUwhDh3Eeq4Y8y7%2Fl1Ysg%3D%3D; SPC_EC=-; SPC_U=-; welcomePkgShown=true; SPC_SI=v7ekrmntpmcy6h182oanzno5s0jv56xg; _gid=GA1.2.1080604625.1579443756; AMP_TOKEN=%24NOT_FOUND; SPC_T_IV="S0+KVDC4g9D9bUnaiGW91w=="; SPC_T_ID="pOZ10jzZj3tI7Su1FwDzOcACMqsiQMDpHjlyR452/wvceCNJF9/gIrGsgjRTG0tZ6plYLTmQqkmLZJA6teOCErd3TY8SjZ9yuNZ7T7uvpFo="'
+Cookie = 'SPC_F=2c95sYFkzfWrz1RZ0pMLLlug1rPm9S0o; REC_T_ID=fc9cb468-d880-11e9-b087-f8f21e1a7612; _ga=GA1.2.741601534.1568638412; __BWfp=c1568638413405xf26e6cced; cto_lwid=b0ccf836-615e-4837-8c84-a7d7c1705a99; SPC_IA=-1; G_ENABLED_IDPS=google; _med=refer; _gcl_au=1.1.122690841.1576505462; _fbp=fb.1.1578399168664.1918442970; csrftoken=9X9IbwZxmTpKJEDtin7wa2sNhJrj72JD; welcomePkgShown=true; SPC_SI=p6knjlgc7xdy9jia9jsa1ulm0b3yx2bk; _gid=GA1.2.876259556.1580033914; SPC_EC="QymejeaKbsVtjHMfnZAg4H2H/sJS9eLE/H9l/F+w2WVnIxr/kHMm4+tvmlTN0cA+LBBrwK18VAFjALDpRgRBXFeARHYKl6yPeHe8swIQsQGZJfbUG20wijq874OKNLb78Z7kbn45cl+53e+bXkeMlR4sTvXDgDK1TliLOinGGeU="; SPC_U=50301774; REC_MD_20=1580044013; AMP_TOKEN=%24NOT_FOUND; _dc_gtm_UA-61915057-6=1; CTOKEN=lY6vfEA8EeqyNMy7%2FvdiIg%3D%3D; SPC_T_IV="dB5+OeSluQzi1xccfp8Dkw=="; SPC_T_ID="pGLwOKt/3aFP55xQBgs2nAhCR/jQqcIzlXIOWWnxaKpbtWdNn5fCg9EHkst/++RIuRgT2NOl5l5trxof6RmmkVQdY9KXs+4hg6Iq00d5ZUk="'
 
-x_csrftoken = "BZqH8wT7HRzYLozH9s4XC3fdPORCycaX"
+x_csrftoken = "9X9IbwZxmTpKJEDti"
 
 User_Agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.117 Safari/537.36"
+
+
+""" Query String Parameters
+by: relevancy
+keyword: google pixel
+limit: 50
+newest: 0
+order: desc
+page_type: search
+version: 2
+"""
